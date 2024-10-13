6cc06bfd7c5cc81bc07b25ecd9f4a5a00ea7152e
Alexis Arancibia <alexisarancibia97@gmail.com>
2024-10-13 15:59:31 -0300

    pequeños cambios en los estilos

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,10 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Alert, Button, SafeAreaView,StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from 'react';
-import { LoginErrors } from '../interfaces/LoginErrors.interface';
+import {LoginErrors} from '../interfaces/LoginErrors.interface';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { RootStackParamList } from '../navigation/RootNavigator';
+import {RootStackParamList} from '../navigation/RootNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
@@ -16,13 +16,13 @@ export function LoginScreen({navigation}: Props): React.JSX.Element {
 
   const validateForm = () => {
     let errors: LoginErrors = {};
-    if (!email) errors.email = "El correo es obligatorio";
-    if (!password) errors.password = "La contraseña es obligatoria";
+    if (!email) errors.email = 'El correo es obligatorio';
+    if (!password) errors.password = 'La contraseña es obligatoria';
 
     setErrors(errors);
 
     return Object.keys(errors).length === 0;
-  }
+  };
 
   const handleSubmit = async () => {
     if (!validateForm()) return;
@@ -30,15 +30,14 @@ export function LoginScreen({navigation}: Props): React.JSX.Element {
     setLoading(true);
 
     try {
-      // Envío solicitud al backend
-      const response = await axios.post('http://10.0.2.2:3000/persona/signin/', {email, password});
+      const response = await axios.post(
+        'http://10.0.2.2:3000/persona/signin/',
+        {email, password},
+      );
       const {token, user} = response.data;
-
-      // Guardo token en AsyncStorage
       const tokenString = JSON.stringify(token);
       await AsyncStorage.setItem('authtoken', tokenString);
 
-      // Limpio los campos
       setEmail('');
       setPassword('');
       setErrors({});
@@ -48,7 +47,10 @@ export function LoginScreen({navigation}: Props): React.JSX.Element {
         if (error.response && error.response.status === 401) {
           Alert.alert('Error', 'Credenciales inválidas. Inténtelo de nuevo.');
         } else {
-          Alert.alert('Error', 'Ha ocurrido un error. Intente nuevamente más tarde.');
+          Alert.alert(
+            'Error',
+            'Ha ocurrido un error. Intente nuevamente más tarde.',
+          );
         }
       } else {
         Alert.alert('Error', 'Ocurrió un error inesperado');
@@ -56,44 +58,56 @@ export function LoginScreen({navigation}: Props): React.JSX.Element {
     } finally {
       setLoading(false);
     }
-
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.form}>
-        <Text style={styles.title}>Iniciar sesión</Text>
-        <TextInput
-          placeholder="Correo"
-          style={styles.input}
-          onChangeText={setEmail}
-          value={email}
-        />
-        {errors.email ? <Text style={styles.errorText}>{errors.email}</Text> : null}
-        <TextInput
-        placeholder="Contraseña"
-        style={styles.input}
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-        />
-        {errors.password ? <Text style={styles.errorText}>{errors.password}</Text> : null}
-        <Text style={styles.link}>Olvidé mi contraseña</Text>
-        <Button title={loading ? "Cargando..." : "Iniciar sesión"} onPress={handleSubmit} disabled={loading}/>
-        <Text style={styles.toRegister}>¿No tenés cuenta? Registrate</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <Text style={styles.title}>Iniciar sesión</Text>
+          <TextInput
+            placeholder="Correo"
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+          />
+          {errors.email ? (
+            <Text style={styles.errorText}>{errors.email}</Text>
+          ) : null}
+          <TextInput
+            placeholder="Contraseña"
+            style={styles.input}
+            secureTextEntry
+            onChangeText={setPassword}
+            value={password}
+          />
+          {errors.password ? (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          ) : null}
+          <Text style={styles.link}>Olvidé mi contraseña</Text>
+          <Button
+            title={loading ? 'Cargando...' : 'Iniciar sesión'}
+            onPress={handleSubmit}
+            disabled={loading}
+          />
+          <Text style={styles.toRegister}>¿No tenés cuenta? Registrate</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea : {
+    flex: 1,
+  },
   container: {
     padding: 24,
     flex: 1,
     justifyContent: 'center',
   },
   errorText: {
-    color: 'red'
+    color: 'red',
   },
   form: {
     marginVertical: 36,
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -2,67 +2,80 @@ import React, {useState} from 'react';
 import {
   ActivityIndicator,
   Button,
+  Dimensions,
   FlatList,
   Modal,
+  SafeAreaView,
   StyleSheet,
   View,
 } from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {InmuebleCard} from '../components/InmuebleCard';
 import {CreateInmuebleForm} from '../components/forms/CreateInmuebleForm';
 import {useInmuebles} from '../hooks/useInmuebles';
 
 export function MainScreen(): React.JSX.Element {
-  const insets = useSafeAreaInsets();
   const [modalVisible, setModalVisible] = useState(false);
   const {inmuebles, loading, loadInmuebles} = useInmuebles();
 
   return (
-    <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
-      <Button title="Agregar inmueble" onPress={() => setModalVisible(true)} />
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}>
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <CreateInmuebleForm
-              onInmuebleCreated={() => {
-                setModalVisible(false);
-                loadInmuebles();
-              }}
-            />
-            <Button
-              title="Cerrar"
-              onPress={() => {
-                setModalVisible(false);
-              }}
-            />
-          </View>
-        </View>
-      </Modal>
-
-      {loading ? (
-        <ActivityIndicator color={'000'} size={'large'} />
-      ) : inmuebles.length === 0 ? (
-        <View>
-          <ActivityIndicator color={'000'} size={'large'} />
-        </View>
-      ) : (
-        <FlatList
-          data={inmuebles}
-          keyExtractor={inmueble => inmueble.id_inmueble.toString()}
-          renderItem={({item}) => (
-            <InmuebleCard inmueble={item} reloadInmuebles={loadInmuebles} />
-          )}
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Button
+          title="Agregar inmueble"
+          onPress={() => setModalVisible(true)}
         />
-      )}
-    </View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <CreateInmuebleForm
+                onInmuebleCreated={() => {
+                  setModalVisible(false);
+                  loadInmuebles();
+                }}
+              />
+              <Button
+                title="Cerrar"
+                onPress={() => {
+                  setModalVisible(false);
+                }}
+              />
+            </View>
+          </View>
+        </Modal>
+
+        {loading ? (
+          <ActivityIndicator color={'000'} size={'large'} />
+        ) : inmuebles.length === 0 ? (
+          <View>
+            <ActivityIndicator color={'000'} size={'large'} />
+          </View>
+        ) : (
+          <FlatList
+            data={inmuebles}
+            keyExtractor={inmueble => inmueble.id_inmueble.toString()}
+            renderItem={({item}) => (
+              <InmuebleCard inmueble={item} reloadInmuebles={loadInmuebles} />
+            )}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
+const {width} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -70,7 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    width: 300,
+    width: width -2,
     padding: 20,
     backgroundColor: 'white',
     borderRadius: 10,
